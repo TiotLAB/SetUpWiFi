c2a2a4cc96f232a01c6ede54b944eb71e583e758
TiotLAB <164747387+TiotLAB@users.noreply.github.com>
2024-12-01 10:13:48 +0700

    Update SetUpWiFi.h

--- SetUpWiFi/include/SetUpWiFi.h
+++ SetUpWiFi/include/SetUpWiFi.h
@@ -14,12 +14,10 @@ class SetUpWiFi {
 private:
   const char *default_ssid;
   const char *default_password;
-  const char *ap_ssid;
-  const char *ap_password;
+  String ap_ssid;
+  String ap_password;
   AsyncWebServer server;
   int buttonPin;
-  int timeCheckWiFi;
-  int timeCheckReset;
   bool wifiConnected = false;
   bool apStarted = false;
 
@@ -31,10 +29,10 @@ private:
 
 public:
   SetUpWiFi();
-  void begin(const char *ssid, const char *password, int buttonPin, int timeCheck);
-  void checkButton(int timeCheckReset);
-  void startCaptivePortal(const char *ap_ssid, const char *ap_password);
-  void handleWiFi(const char *ap_ssid, const char *ap_password);
+  void begin(const char *ssid, const char *password, int buttonPin, int timeCheck = 30000);
+  void checkButton(int timeCheckReset = 2000);
+  void startCaptivePortal(const char *ap_ssid = nullptr, const char *ap_password = nullptr);
+  void handleWiFi(const char *ap_ssid = nullptr, const char *ap_password = nullptr);
 };
 
 #endif // SET_UP_WIFI_H
